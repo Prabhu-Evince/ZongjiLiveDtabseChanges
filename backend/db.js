9792d12e0b991976b117dc80474693e5aa1ab504
Prabhu Panda <prabhu@evincedev.com>
2025-11-14 11:30:22 +0530

    Db notifier db.js fixes

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const knex = require('knex');
 const db = knex({
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'Prabhuev@123',
+    database: 'webzongji'
   },
   pool: { min: 2, max: 10 }
 });
